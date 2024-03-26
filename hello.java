fec94947ac4fe9d2322feb74a6e4ac515d4e6339
xhl <3424976485@qq.com>
2024-03-26 15:17:51 +0800

    第一次提交

---
+++ hello.java
@@ -0,0 +1,18 @@
+package mystudy;
+
+public class day01 {
+    public static void main(String[] args) {
+
+        String name = "肖海林";
+        char gender = '男';
+        int age = 20;
+        double height = 175;
+        boolean flag = false;
+
+        System.out.println(name);
+        System.out.println(gender);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(flag);
+    }
+}
